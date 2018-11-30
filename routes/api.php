b3c7e6ea89d432c3c71cb5c1b5dec3ef02a6df22
trioputrap <trioputrap@gmail.com>
2018-11-30 16:18:22 +0800

    testing

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Http\Request;
 */
 
 Route::post('login', 'API\UserController@login');
+Route::get('login', 'API\UserController@login');
 Route::post('register', 'API\UserController@register');
 
 Route::group(['middleware' => 'auth:api'], function(){
