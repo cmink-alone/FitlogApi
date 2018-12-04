40f532b33673edb5c71bec220c074ac4d664525f
trioputrap <trioputrap@gmail.com>
2018-12-04 23:40:41 +0800

    activity controller

--- app/Http/Controllers/API/ActivityController.php
+++ app/Http/Controllers/API/ActivityController.php
@@ -26,7 +26,7 @@ public $successStatus = 200;
         $user_ids = $user->followings->pluck('following_id')->toArray();
         $user_ids[]=$user->id;
 
-        $activities = Activity::whereIn("user_id",$user_ids)->get(['id AS server_id', '*']); 
+        $activities = Activity::whereIn("user_id",$user_ids)->get(['id AS server_id', 'activities.*']); 
         return response()->json($activities, $this-> successStatus); 
     } 
 
