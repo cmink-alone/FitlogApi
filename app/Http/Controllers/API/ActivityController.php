77951524789bf6c92cdbf9ea6b5526d93d7c75ca
trioputrap <trioputrap@gmail.com>
2018-12-18 10:54:41 +0800

    following

--- app/Http/Controllers/API/ActivityController.php
+++ app/Http/Controllers/API/ActivityController.php
@@ -23,7 +23,7 @@ public $successStatus = 200;
     { 
         $user = Auth::user(); 
         
-        $user_ids = $user->followings->pluck('following_id')->toArray();
+        $user_ids = $user->following->pluck('following_id')->toArray();
         $user_ids[]=$user->id;
 
         $activities = Activity::whereIn("user_id",$user_ids)->get(['id AS server_id', 'activities.*']); 
